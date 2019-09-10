498a8e419bd8a0165bc079e288ddfe8647bba1c8
Andy DuBois <andydubois89@gmail.com>
2019-09-10 16:16:48 -0500

    set up view/add bands with columns

--- src/components/ViewAddBands/ViewAddBands.jsx
+++ src/components/ViewAddBands/ViewAddBands.jsx
@@ -1,12 +1,41 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//Material UI Components
+import { withStyles } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  }
+});
 
 class ViewAddBands extends Component {
-    render() {
-        return (
-            <p>This is the View/add Bands Page!</p>
-        )
-    }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <h1>View/Add Bands</h1>
+        <div className={classes.root}>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <Paper>xs=12</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>xs=12</Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default connect()(ViewAddBands);
+const mapStateToProps = store => ({
+  store
+});
+
+export default withStyles(styles)(connect(mapStateToProps)(ViewAddBands));
